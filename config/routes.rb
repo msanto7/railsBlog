2bd6fb19f6ef5e6ba599b39798b755d8d7d0f6ea
Mike Santoro <mike.santoro42@gmail.com>
2019-05-31 23:09:04 -0400

    adding controllers to send basic data to the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  # root Route
+  root 'posts#index'
+
+  # Regular Routes
+  get 'about' => 'pages#about'
+
+
+
 end
